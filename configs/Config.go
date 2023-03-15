1c16c7299f97e92d5b41f31761669b87ba8c0b8b
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2023-03-15 17:24:30 -0400

    Implementing default values for the optional environment variables

--- configs/Config.go
+++ configs/Config.go
@@ -1,28 +1,22 @@
 package configs
 
 import (
-	"os"
+	"github.com/c3s4rfred/sforceds/utils"
 	"time"
 )
 
 const (
 	// OAuth server variables
-	OAuthService     = "https://login.salesforce.com"
-	LoginEndpoint    = "/services/oauth2/token"
 	OAuthDialTimeout = 15 * time.Second
 	// OAuth header variables
 	GrantType = "password"
-	// Events endpoint
-	EventsEndPoint = "/services/data/v57.0/sobjects/EventLogFile"
-	// Query endpoint
-	QueryEndPoint   = "/services/data/v57.0/query?q=SELECT%20Id,LastModifiedDate%20FROM%20EventLogFile%20WHERE%20LastModifiedDate%20>%20"
+	// Query definition of the query used to complete QueryEndPoint value
+	Query           = "?q=SELECT%20Id,LastModifiedDate%20FROM%20EventLogFile%20WHERE%20LastModifiedDate%20>%20"
 	OrderByForQuery = "%20ORDER%20BY%20Id%20ASC"
 	// Salesforce constants for json
 	SalesForce_dataType = "sales-force"
-	// SIEM URL
-	SiemURL = "http://correlation:8080/v1/newlog"
 	// Actual version of the API
-	SF_version = "1.0.0 2023-03-14 15:08:24"
+	SF_version = "1.0.0 2023-03-15 16:58:06"
 )
 
 // State constants
@@ -35,10 +29,22 @@ const (
 
 // Sales force login params
 var (
-	ClientId      = os.Getenv("clientID")
-	ClientSecret  = os.Getenv("clientSecret")
-	Username      = os.Getenv("username")
-	Password      = os.Getenv("password")
-	SecurityToken = os.Getenv("securityToken")
-	InstanceUrl   = os.Getenv("instanceUrl")
+	// SalesForce connection parameters
+	ClientId      = utils.GetEnvOrElse("clientID", "not set")
+	ClientSecret  = utils.GetEnvOrElse("clientSecret", "not set")
+	Username      = utils.GetEnvOrElse("username", "not set")
+	Password      = utils.GetEnvOrElse("password", "not set")
+	SecurityToken = utils.GetEnvOrElse("securityToken", "not set")
+	InstanceUrl   = utils.GetEnvOrElse("instanceUrl", "not set")
+	// OAuth server variables
+	// OAuthService represents the salesforce login service url
+	OAuthService = utils.GetEnvOrElse("OAuthService", "https://login.salesforce.com")
+	// LoginEndpoint represents the salesforce endpoint that returns the oauth2 token based on credentials
+	LoginEndpoint = utils.GetEnvOrElse("LoginEndpoint", "/services/oauth2/token")
+	// EventsEndPoint represents the salesforce endpoint of the event log files, to get the data by Id
+	EventsEndPoint = utils.GetEnvOrElse("EventsEndPoint", "/services/data/v57.0/sobjects/EventLogFile")
+	// QueryEndPoint represents the salesforce endpoint to query and return all the event log files endpoints
+	QueryEndPoint = utils.GetEnvOrElse("QueryEndPoint", "/services/data/v57.0/query") + Query
+	// SIEM URL -> Logs destination
+	SiemURL = utils.GetEnvOrElse("siemURL", "http://correlation:8080/v1/newlog")
 )
